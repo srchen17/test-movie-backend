6f9628c10731320c7b94717a5df553536c7dfc7f
srchen17 <118634599+srchen17@users.noreply.github.com>
2023-11-18 20:03:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from './lab5.js';
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from './modules/routes.js';
+import "dotenv/config.js"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
+
