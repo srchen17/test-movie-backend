bbf94ca3baf29686200aa94c844d5e4d2cfa2aae
srchen17 <118634599+srchen17@users.noreply.github.com>
2023-11-19 19:45:03 -0500

    fixed todo

--- lab5.js
+++ lab5.js
@@ -10,7 +10,7 @@ const todos = [
   { id: 1, title: "Task 1", completed: false },
   { id: 2, title: "Task 2", completed: false },
   { id: 3, title: "Task 3", completed: false },
-  { id: 4, title: "Task 4", completed: true }, // false 
+  { id: 4, title: "Task 4", completed: false }, // false 
 ];
 
 
