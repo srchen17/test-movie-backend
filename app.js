1d23a9c0ddc1c28a0135eaad365a665e1ea92e97
srchen17 <118634599+srchen17@users.noreply.github.com>
2023-11-30 16:34:27 -0500

    a6

--- app.js
+++ app.js
@@ -3,12 +3,52 @@ import Hello from "./hello.js";
 import Lab5 from './lab5.js';
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
+import mongoose from 'mongoose';
+import UserRoutes from "./users/routes.js";
+import "dotenv/config"; 
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
+
 import ModuleRoutes from './modules/routes.js';
 import "dotenv/config.js"
+import session from 'express-session';
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+      }     
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+//   };
+//   app.use(
+//     session(sessionOptions)
+//   );
+  
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
