04eae6bf54c6b6fc3dd2e93661acccef976b0693
srchen17 <118634599+srchen17@users.noreply.github.com>
2023-12-10 17:51:41 -0500

    changed url to .env

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import ReviewRoutes from './reviews/routes.js';
 
-mongoose.connect("mongodb://127.0.0.1:27017/movie");
+import "dotenv/config"; 
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/movie';
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(express.json());
 app.use(
